d13a4bbe8c7fd70f8e0a066af95a16fcca725c14
zadumka <alenahas1987@gmail.com>
2025-06-03 18:05:17 +0300

    add hw

---
+++ components/Header/Header.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+import css from './Header.module.css';
+import TagsMenu from '../TagsMenu/TagsMenu';
+
+export default function Header() {
+  return (
+    <header className={css.header}>
+      <Link href="/" aria-label="Home" className={css.headerLink}>
+        NoteHub
+      </Link>
+      <nav aria-label="Main Navigation">
+        <ul className={css.navigation}>
+          <li className={css.navigationItem}>
+            <Link className={css.navigationLink} href="/">
+              Home
+            </Link>
+          </li>
+          <li className={css.navigationItem}>
+            <TagsMenu />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
